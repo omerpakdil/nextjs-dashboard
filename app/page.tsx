4c68f9da6998c31b3bcfc38c94523b89d3a47f7d
Omer Salih Pakdil <omerp@mobiliz.com.tr>
2024-06-05 15:34:14 +0300

    changes on text

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
             className={styles.shape}
           />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to My Course.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
